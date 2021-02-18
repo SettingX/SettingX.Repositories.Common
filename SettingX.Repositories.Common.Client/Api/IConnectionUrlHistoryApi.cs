2fd4eb1c8d79e3156495c05d97252bb56e62849b
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-18 15:04:53 +0400

    IConnectionUrlHistoryApi changes.

--- SettingX.Repositories.Common.Client/Api/IConnectionUrlHistoryApi.cs
+++ SettingX.Repositories.Common.Client/Api/IConnectionUrlHistoryApi.cs
@@ -7,15 +7,15 @@ namespace SettingX.Repositories.Common.Client.Api
 {
     public interface IConnectionUrlHistoryApi
     {
-        [Post("/api/connection_url_history")]
+        [Get("/api/connection_url_history")]
         Task<(List<ConnectionUrlHistoricEvent>, int)> GetPageAsync([Query] int pageNum, [Query] int pageSize);
         
         [Get("/api/connection_url_history/{repositoryId}")]
         Task<List<ConnectionUrlHistoricEvent>> GetByRepositoryIdAsync(string repositoryId);
         
-        [Post("/api/connection_url_history")]
+        [Post("/api/connection_url_history/{repositoryId}")]
         Task SaveConnectionUrlHistoryAsync(
-            [Query] string repositoryId,
+            string repositoryId,
             [Query] string ip,
             [Query] string userAgent);
     }
