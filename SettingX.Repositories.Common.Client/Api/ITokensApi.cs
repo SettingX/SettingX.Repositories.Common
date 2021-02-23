b1af6bd870f262c484511db84eebe346be1d3c9b
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-23 11:15:00 +0400

    Tokens api fixed.

--- SettingX.Repositories.Common.Client/Api/ITokensApi.cs
+++ SettingX.Repositories.Common.Client/Api/ITokensApi.cs
@@ -7,19 +7,19 @@ namespace SettingX.Repositories.Common.Client.Api
 {
     public interface ITokensApi
     {
-        [Get(("/api/tokens/{tokenId}"))]
+        [Get("/api/tokens/{tokenId}")]
         Task<Token> GetAsync(string tokenId);
         
-        [Get(("/api/tokens/top"))]
+        [Get("/api/tokens/top")]
         Task<Token> GetTopRecordAsync();
         
-        [Get(("/api/tokens"))]
+        [Get("/api/tokens")]
         Task<List<Token>> GetAllAsync();
 
-        [Delete(("/api/tokens/{tokenId}"))]
+        [Delete("/api/tokens/{tokenId}")]
         Task RemoveTokenAsync(string tokenId);
         
-        [Post(("/api/tokens"))]
+        [Post("/api/tokens")]
         Task SaveTokenAsync([Body] Token token);
     }
 }
