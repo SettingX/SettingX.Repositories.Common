c9defcc8af20666ca87b87b1f536a96312b2a7d3
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-23 10:55:35 +0400

    Services added.

--- SettingX.Repositories.Common.Client/Client.cs
+++ SettingX.Repositories.Common.Client/Client.cs
@@ -7,11 +7,43 @@ namespace SettingX.Repositories.Common.Client
     public class Client : IClient
     {
         public IRepositoryDataApi RepositoryDataApi { get; }
+        public IAccountTokenHistoryApi AccountTokenHistoryApi { get; }
+        public IConnectionUrlHistoryApi ConnectionUrlHistoryApi { get; }
+        public IKeyValuesApi KeyValuesApi { get; }
+        public IKeyValuesHistoryApi KeyValuesHistoryApi { get; }
+        public ILocksApi LocksApi { get; }
+        public INetworksApi NetworksApi { get; }
+        public IRepositoriesApi RepositoriesApi { get; }
+        public IRepositoriesUpdateHistoryApi RepositoriesUpdateHistoryApi { get; }
+        public IRolesApi RolesApi { get; }
+        public ISecretKeyValuesApi SecretKeyValuesApi { get; }
+        public IServiceTokenHistoryApi ServiceTokenHistoryApi { get; }
+        public IServiceTokensApi ServiceTokensApi { get; }
+        public ITokensApi TokensApi { get; }
+        public IUserActionsHistoryApi UserActionsHistoryApi { get; }
+        public IUsersApi UsersApi { get; }
+        public IUserSignInHistoryApi UserSignInHistoryApi { get; }
         
         public Client(string rootUrl, JsonSerializerOptions jsonSerializerOptions)
         {
             var settings = new RefitSettings(new SystemTextJsonContentSerializer(jsonSerializerOptions));
             RepositoryDataApi = RestService.For<IRepositoryDataApi>(rootUrl, settings);
+            AccountTokenHistoryApi = RestService.For<IAccountTokenHistoryApi>(rootUrl, settings);
+            ConnectionUrlHistoryApi = RestService.For<IConnectionUrlHistoryApi>(rootUrl, settings);
+            KeyValuesApi = RestService.For<IKeyValuesApi>(rootUrl, settings);
+            KeyValuesHistoryApi = RestService.For<IKeyValuesHistoryApi>(rootUrl, settings);
+            LocksApi = RestService.For<ILocksApi>(rootUrl, settings);
+            NetworksApi = RestService.For<INetworksApi>(rootUrl, settings);
+            RepositoriesApi = RestService.For<IRepositoriesApi>(rootUrl, settings);
+            RepositoriesUpdateHistoryApi = RestService.For<IRepositoriesUpdateHistoryApi>(rootUrl, settings);
+            RolesApi = RestService.For<IRolesApi>(rootUrl, settings);
+            SecretKeyValuesApi = RestService.For<ISecretKeyValuesApi>(rootUrl, settings);
+            ServiceTokenHistoryApi = RestService.For<IServiceTokenHistoryApi>(rootUrl, settings);
+            ServiceTokensApi = RestService.For<IServiceTokensApi>(rootUrl, settings);
+            TokensApi = RestService.For<ITokensApi>(rootUrl, settings);
+            UserActionsHistoryApi = RestService.For<IUserActionsHistoryApi>(rootUrl, settings);
+            UsersApi = RestService.For<IUsersApi>(rootUrl, settings);
+            UserSignInHistoryApi = RestService.For<IUserSignInHistoryApi>(rootUrl, settings);
         }
     }
 }
--- SettingX.Repositories.Common.Client/IClient.cs
+++ SettingX.Repositories.Common.Client/IClient.cs
@@ -5,5 +5,21 @@ namespace SettingX.Repositories.Common.Client
     public interface IClient
     {
         IRepositoryDataApi RepositoryDataApi { get; }
+        IAccountTokenHistoryApi AccountTokenHistoryApi { get; }
+        IConnectionUrlHistoryApi ConnectionUrlHistoryApi { get; }
+        IKeyValuesApi KeyValuesApi { get; }
+        IKeyValuesHistoryApi KeyValuesHistoryApi { get; }
+        ILocksApi LocksApi { get; }
+        INetworksApi NetworksApi { get; }
+        IRepositoriesApi RepositoriesApi { get; }
+        IRepositoriesUpdateHistoryApi RepositoriesUpdateHistoryApi { get; }
+        IRolesApi RolesApi { get; }
+        ISecretKeyValuesApi SecretKeyValuesApi { get; }
+        IServiceTokenHistoryApi ServiceTokenHistoryApi { get; }
+        IServiceTokensApi ServiceTokensApi { get; }
+        ITokensApi TokensApi { get; }
+        IUserActionsHistoryApi UserActionsHistoryApi { get; }
+        IUsersApi UsersApi { get; }
+        IUserSignInHistoryApi UserSignInHistoryApi { get; }
     }
 }
